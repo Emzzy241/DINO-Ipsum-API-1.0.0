223f5f50640103eafb6d09077c77ae10e3b4b289
Emzzy241 <emzzyoluwole@gmail.com>
2022-11-27 15:19:01 +0100

    adding my javascript file

---
+++ src/main.js
@@ -0,0 +1,85 @@
+// starting application and doing all of it in one file because that's what the version 1.0.0 is all about --NO separation of concern, logic(business and UI), check out version 1.1.0
+
+
+// this is my webpack entry point file, I need to import all what I would be using for the APPlication first
+import $ from "jquery";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/styles.css";
+
+
+// importing the icon image and dino image
+import jsImage from "./assets/images/js-badge.svg";
+import dinoImage from "./assets/images/dno1.png";
+
+
+$(document).ready(() =>{
+
+    let iconImg = $(".appImg");
+    iconImg.attr("href", jsImage)
+
+    $(".name").text("hola, me name is Emmanuel");
+
+    let dinoImg = $(".dinoImg");
+    dinoImg.attr("src", dinoImage);
+
+    // working with my api call when users submit the form
+    
+    $("#dinoForm").submit((event) =>{
+        // preventing submit button from refreshing the page
+        event.preventDefault();
+
+        // showing the result div when form is submitted
+        $(".result").show();
+
+        // getting the number of dinosaur paragraph user wants
+        let noOfDinosaursParagraph = $("#numParagraphs").val();
+        
+        
+        let noOfDinosaursNames = $("#numWords").val();
+
+        // after taking in my values, its time to make my api call
+
+        // reinstantiating a new xmlhttp request
+        let myRequest = new XMLHttpRequest();
+
+        // my api call endpoint
+        let myEndpointUrl = `https://dinoipsum.com/api/?format=json&words=${noOfDinosaursNames}&paragraphs=${noOfDinosaursParagraph}`;
+
+
+        // time to work with api requests
+        myRequest.onreadystatechange = function (){
+            // telling javascript when my api call response would be ready
+            if(this.readyState === 4 && this.status === 200){
+                // parsing the json response gotten because I did set my apicall to give a response in json format
+                // also the .parse() method is capable of turning javascript object notation(JSON) into javascript objects
+                const dinoResponse = JSON.parse(this.responseText);
+
+
+                // calling a function that gets dinos for users to get executed when my request was successful
+                // also while caling this function, I passed in my dinoResponse variable storing the converted javascript object[from json to javascript objects all thanks to JSON.parse();]
+                getDinos(dinoResponse);
+            }
+        }
+        // final steps of my call; opening a get request, with the variable I stored my Endpoint in and lastly a boolean that will determine whether I want my request opened or not
+
+
+        myRequest.open("GET", myEndpointUrl, true);
+        myRequest.send();
+
+        // time to write the function I called earlier on that would get executed when my apiCall request was successful
+
+        function getDinos(getMeDinosaurs){
+            $(".dOne").text(`${getMeDinosaurs[0][0]}`)
+            $(".dTwo").text(`${getMeDinosaurs[0][0]}`)
+            $(".dThree").text(`${getMeDinosaurs[0][0]}`)
+            $(".dFour").text(`${getMeDinosaurs[0][0]}`)
+        }
+
+    });
+
+
+});
+
+// the endpoint to use: https://dinoipsum.com/api/?format=json&words=6&paragraphs=3
+// in the endpoint you can edit both paragraphs and words
